ea6a7669416874c5e93e8b6441b4580aded5a48d
12tristan12 <imala@ut.ee>
2025-03-19 21:17:53 +0200

    Lisasin Seats klassi ning tegin vajlikud muutused(SeatRepository jne.), et genereeriks igale lennule automaatselt 150 uut istet

--- src/main/java/com/CGIpraktika/lennu_planeerija/service/FlightService.java
+++ src/main/java/com/CGIpraktika/lennu_planeerija/service/FlightService.java
@@ -1,19 +1,24 @@
 package com.CGIpraktika.lennu_planeerija.service;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.CGIpraktika.lennu_planeerija.model.Flight;
+import com.CGIpraktika.lennu_planeerija.model.Seats;
 import com.CGIpraktika.lennu_planeerija.repository.FlightRepository;
+import com.CGIpraktika.lennu_planeerija.repository.SeatRepository;
 
 @Service
 public class FlightService {
     private final FlightRepository flightRepository;
+    private final SeatRepository seatRepository;
 
-    public FlightService(FlightRepository flightRepository) {
+    public FlightService(FlightRepository flightRepository, SeatRepository seatRepository) {
         this.flightRepository = flightRepository;
+        this.seatRepository = seatRepository;
     }
 
 
@@ -23,6 +28,20 @@ public class FlightService {
 
 
     public Flight addFlight(Flight flight) {
-        return flightRepository.save(flight);
+        Flight savedFlight = flightRepository.save(flight);
+        generateSeatsForFlight(savedFlight, 150); 
+        return savedFlight;
+    }
+
+    private void generateSeatsForFlight(Flight flight, int seatCount) {
+        List<Seats> seats = new ArrayList<>();
+        for (int i = 1; i <= seatCount; i++) {
+            Seats seat = new Seats();
+            seat.setSeatNumber("A" + i); 
+            seat.setIsBooked(false);
+            seat.setFlight(flight); 
+            seats.add(seat);
+        }
+        seatRepository.saveAll(seats);
     }
 }
